c9f9a40cf40e84a5c1f6c71843195b3c4882f5e4
Aaron Huang <ahuan@bu.edu>
2025-09-25 11:01:12 -0400

    added docstrings + formating to comply to coding conventions

--- mini_insta/models.py
+++ mini_insta/models.py
@@ -6,12 +6,25 @@ from django.db import models
 
 # Create your models here.
 class Profile(models.Model):
-    username = models.TextField(blank = True)
-    display_name = models.TextField(blank = True)
-    profile_image_url = models.URLField(blank = True)
-    bio_text = models.TextField(blank = True)
-    join_date = models.DateTimeField(auto_now = True)
+    """
+    Represents a user profile in the Mini Instagram application.
+
+    Fields:
+        username (TextField): The user's unique identifier.
+        display_name (TextField): The name displayed publicly.
+        profile_image_url (URLField): Link to the user's profile image.
+        bio_text (TextField): Short biography or description.
+        join_date (DateTimeField): Timestamp of when the profile was created.
+
+    Methods:
+        __str__: Returns a string representation combining username and join date.
+    """
+    username = models.TextField(blank=True)
+    display_name = models.TextField(blank=True)
+    profile_image_url = models.URLField(blank=True)
+    bio_text = models.TextField(blank=True)
+    join_date = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        '''return a string representation of this model instance.'''
-        return f'{self.username} '
+        """return a string representation of this model instance."""
+        return f'{self.username} joined on {self.join_date.strftime("%b %d, %Y")}'
--- mini_insta/views.py
+++ mini_insta/views.py
@@ -8,11 +8,27 @@ from django.views.generic import ListView, DetailView
 # Create your views here.
 
 class ProfileListView(ListView):
+    """
+    Displays a list of all Profile instances.
+
+    Attributes:
+        model (Profile): The model to query from the database.
+        template_name (str): Path to the HTML template that renders the list.
+        context_object_name (str): The name used to reference the list of profiles in the template.
+    """
     model = Profile
     template_name = "mini_insta/show_all_profiles.html"
     context_object_name = "profiles"
     
 class ProfileDetailView(DetailView):
+    """
+    Displays detailed information for a single Profile instance.
+
+    Attributes:
+        model (Profile): The model to query from the database.
+        template_name (str): Path to the HTML template that renders the profile detail.
+        context_object_name (str): The name used to reference the profile object in the template.
+    """
     model = Profile
     template_name = "mini_insta/show_profile.html"
     context_object_name = "profile"
