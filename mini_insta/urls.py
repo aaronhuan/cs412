0d6aea4daec3c64c7bb4936eb627d38a194565b5
Aaron Huang <ahuan@bu.edu>
2025-09-25 11:08:41 -0400

    fixed naming for url

--- mini_insta/urls.py
+++ mini_insta/urls.py
@@ -6,5 +6,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path("", views.ProfileListView.as_view(), name="show_all_profiles"),
-    path("profile/<int:pk>", views.ProfileDetailView.as_view(), name="show_one_profile"),
+    path("profile/<int:pk>", views.ProfileDetailView.as_view(), name="show_profile"),
 ]
