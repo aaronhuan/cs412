0a96a2042578fa3268bd4a60e0f633014c1eb4b9
Aaron Huang <ahuan@bu.edu>
2025-11-01 13:41:20 -0400

    url modification

--- cs412/urls.py
+++ cs412/urls.py
@@ -25,7 +25,7 @@ urlpatterns = [
     path('restaurant/', include('restaurant.urls')),
     path('blog/', include("blog.urls")),
     path('mini_insta/', include("mini_insta.urls")),
-    path('voters/', include('voter_analytics.urls')),
+    path('voter_analytics/', include('voter_analytics.urls')),
 ]
 
 urlpatterns+= static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
